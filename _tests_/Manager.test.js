e48662cf6bc040d96fe0045469629ce657be84e3
Ysfboi <jdanleano@gmail.com>
2022-08-11 23:21:32 -0400

    Fixed tests issues. Also added screenshots

--- _tests_/Manager.test.js
+++ _tests_/Manager.test.js
@@ -3,14 +3,14 @@ const Manager = require('../lib/Manager');
 
 // creating manager object  
 test('creates an Manager object', () => {
-    const manager = new Manager('Nicole', 90, 'nicole.elisaw@gmail', 4);
+    const manager = new Manager('Joseph', 12345, 'joseph@email.com', 1234567890);
     
     expect(manager.officeNumber).toEqual(expect.any(Number));
 });
 
 // gets role from getRole()
 test('gets role of employee', () => {
-    const manager = new Manager('Nicole', 90, 'nicole.elisaw@gmail.com');
+    const manager = new Manager('Joseph', 12345, 'joseph@email.com');
 
     expect(manager.getRole()).toEqual("Manager");
 }); 
